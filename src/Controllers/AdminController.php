a9eedeabc4664638a35c57f675b493b79158e4fc
wargen41 <106013425+wargen41@users.noreply.github.com>
2026-04-19 13:45:41 +0200

    Update AdminController.php

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -681,6 +681,7 @@ class AdminController
             $limit = $params['limit'] ?? '-1';
             $search = $params['search'] ?? null;
             $type = $params['type'] ?? null;
+            {# Att kunna välja Alla funkar inte när jag har sidor uppdelade mellan filmer och serier #}
             if($type == 'all'){
                 $type = null;
             }else if($type == null){
