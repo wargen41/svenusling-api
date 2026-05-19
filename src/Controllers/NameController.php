6b0e7c38aeb9e09ed5216f68d84a11859743c2e2
wargen41 <106013425+wargen41@users.noreply.github.com>
2026-05-19 21:10:48 +0200

    Update NameController.php

--- src/Controllers/NameController.php
+++ src/Controllers/NameController.php
@@ -27,7 +27,6 @@ class NameController
                 return $this->jsonResponse($response, ['error' => 'Invalid name'], 400);
             }
 
-            // Work in progress
             $stmt = $this->db->prepare('
             SELECT mp.person_id, mp.category, mp.role_name, mp.note, mp.movie_id, m.title AS movie_title, m.year AS movie_year
             FROM movies_persons mp
@@ -37,11 +36,21 @@ class NameController
             $stmt->execute([$name]);
             $crew = $stmt->fetchAll();
 
+            $stmt = $this->db->prepare('
+            SELECT mt.*, m.title AS movie_title, m.year AS movie_year
+            FROM movies_trivia mt
+            JOIN movies m ON mt.movie_id = m.id
+            WHERE (mt.sv LIKE ? OR mt.en LIKE ?)
+            ');
+            $stmt->execute([$name, $name]);
+            $trivia = $stmt->fetchAll();
+
             return $this->jsonResponse($response, [
                 'success' => true,
                 'data' => [
                     'name' => $name,
                     'crew' => $crew,
+                    'trivia' => $trivia,
                 ]
             ]);
 
